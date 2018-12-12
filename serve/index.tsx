c36f5c0441954f149eca7b5ee6b270d2f1fc2772
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2018-12-12 23:10:45 +0330

    change serve package approach

--- serve/index.tsx
+++ serve/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { DatePicker } from "../dist/index";
+import { DatePicker } from "../src/index";
 
 render(<DatePicker />, document.getElementById("root"));
