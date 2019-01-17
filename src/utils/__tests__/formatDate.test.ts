bec32c9b5aa3f22a4b925778a722997a168e5861
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-01-17 19:05:26 +0330

    add date formater

---
+++ src/utils/__tests__/formatDate.test.ts
@@ -0,0 +1,6 @@
+import { formatJalaliDate } from "../formatDate";
+
+test("format date jalali string to moment ", () => {
+  const format = formatJalaliDate("1397/05/18");
+  expect(format.format("jYYYY/jMM/jDD")).toBe("1397/05/18");
+});
