e2abeeed9d22e4a038f284fd96c99dbb05163461
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-01-17 21:10:30 +0330

    update serve file

--- serve/index.tsx
+++ serve/index.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
 import { render } from "react-dom";
-import { DatePicker } from "../dist/index";
+import { RangeDatePicker } from "../dist/index";
 
-function test(dish) {
-  console.log("dish ", dish);
-}
-render(<DatePicker daysOnClick={test} />, document.getElementById("root"));
+render(<RangeDatePicker />, document.getElementById("root"));
