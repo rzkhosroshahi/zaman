1ee7a07fa2b7c179bb81c38ef3deea5ed63c8adf
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-01-25 17:33:19 +0330

    solve rangeHelper test issue

--- src/utils/__tests__/rangeHelper.test.ts
+++ src/utils/__tests__/rangeHelper.test.ts
@@ -1,7 +1,9 @@
 import { rangeHelper } from "../rangeHelper";
 import * as moment from "jalali-moment";
+import { cleanup } from "react-testing-library";
 
 describe("rangeHelper test ", () => {
+  afterEach(cleanup);
   test("range acceptance test ", () => {
     const range = {
       start: moment("Tue, 25 Dec 2018 20:01:44 GMT"),
