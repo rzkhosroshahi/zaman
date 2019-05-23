698d3d64e42134ff71ef71b2ae1b2a2ce57d4e73
Reza Khosroshahi <rzkhosroshahi@gmail.com>
2019-05-23 14:49:08 +0430

    remove the redundant exported file from the index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,2 @@
 export * from "./components/RangeDatePicker";
 export * from "./components/DatePicker";
-export { RangeDateDiv } from "./components/RangeDatePicker/styled";
-export { DatePickerDiv } from "./components/DatePicker/styled";
