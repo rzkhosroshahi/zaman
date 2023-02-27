f4f3c71ca796b56462c4e6cee5d419a6e5aa45f5
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2023-02-27 17:05:04 +0330

    chore: add packages

---
+++ src/packages/DatePicker/DatePicker.constants.ts
@@ -0,0 +1,1 @@
+export const ITEMS_WIDTH = 324
---
+++ src/packages/DatePicker/DatePicker.styled.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled'
+import { ITEMS_WIDTH } from '../../constants'
+
+export const Container = styled.div`
+  position: relative;
+  max-width: ${ITEMS_WIDTH}px;
+`
+
+export const Wrapper = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: ${ITEMS_WIDTH}px;
+  min-height: 240px;
+  border: 1px solid #cccc;
+  border-radius: 16px;
+`
+
+export const WrapperDays = styled.div`
+  display: flex;
+  position: absolute;
+  overflow: hidden;
+`
+
+export const Day = styled.div`
+  will-change: transform;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${ITEMS_WIDTH}px;
+  min-height: 240px;
+  background-color: wheat;
+`
