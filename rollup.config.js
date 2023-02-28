cc866f31dc4478b2ae9b5b2c777dc5ad7f723eba
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2023-02-28 11:39:48 +0330

    chore: add babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,24 @@
 import typescript from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
-
+import babel from '@rollup/plugin-babel'
 export default {
   input: './src/index.tsx',
-  output: {
-    file: './dist/index.js',
-    format: 'es'
-  },
+  output: [
+    {
+      file: './dist/index.js',
+      format: 'es'
+    },
+    {
+      file: './dist/index.cjs.js',
+      format: 'cjs'
+    }
+  ],
   plugins: [
     typescript({ tsconfig: './tsconfig.json' }),
     resolve({
       browser: true
-    })
+    }),
+    babel({ babelHelpers: 'bundled' })
   ],
   external: [
     'react',
