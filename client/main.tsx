eb4d794e4bea9c80169955bccfd5940d0cbe8760
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2023-03-11 13:10:01 +0330

    chore: use strictmode in client

--- client/main.tsx
+++ client/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { DatePicker } from '../dist'
+import { DatePicker } from '../src'
 
 import { createRoot } from 'react-dom/client'
 const container = document.getElementById('root')
 const root = createRoot(container as HTMLElement)
-root.render(<DatePicker round="roundX2" accentColor="#32c561" />)
+
+root.render(
+  <React.StrictMode>
+    <DatePicker round="roundX2" accentColor="#32c561" />
+  </React.StrictMode>
+)
