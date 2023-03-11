57d46d3984f7abb88e41040488356c3d941cfdab
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2023-03-11 13:37:41 +0330

    chore: remove unused files

--- serve/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import App from '../dist/index.js'
-import { createRoot } from 'react-dom/client'
-
-const container = document.getElementById('app')
-const root = createRoot(container)
-root.render(<App />)
