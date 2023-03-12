7055fc6eda0096befd70748c364081c8c97eb8d6
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2023-03-12 21:26:36 +0330

    chore: improve client

--- client/main.tsx
+++ client/main.tsx
@@ -9,7 +9,7 @@ const App = () => {
   const [view, setView] = useState<string>('day')
 
   return (
-    <section>
+    <section className="wrapper">
       <div className="tab">
         <div
           className={`tabItem tabItem${view === 'cal' ? '--selected' : ''}`}
@@ -24,43 +24,37 @@ const App = () => {
           Date picker
         </div>
       </div>
-      {
-        view === 'cal'
-          ? <div style={{ display: 'flex', justifyContent: 'center', gap: '14px' }}>
-            <CalendarProvider locale="fa" round="roundX2">
-              <Calendar
-                value={new Date()}
-                onChange={(d) => console.log(d)}
-                weekends={[6]}
-              />
-            </CalendarProvider>
-            <CalendarProvider locale="fa" round="roundX4" accentColor="#6374ae">
-              <Calendar
-                value={new Date()}
-                onChange={(d) => console.log(d)}
-                weekends={[6]}
-                from="Mon Mar 06 2023 20:11:58 GMT+0330 (Iran Standard Time)"
-                to="Wed Mar 14 2023 21:41:23 GMT+0330 (Iran Standard Time)"
-                range
-              />
-            </CalendarProvider>
-          </div>
-          : null
-      }
-      {
-        view === 'day'
-          ? <div style={{ display: 'flex', justifyContent: 'center', gap: '14px' }}>
-            <DatePicker
-              round="roundX4"
+      {view === 'cal'
+        ? (
+        <div className="libWrapper">
+          <CalendarProvider locale="fa" round="roundX2">
+            <Calendar
+              value={new Date()}
+              onChange={(d) => console.log(d)}
+              weekends={[6]}
             />
-            <DatePicker
-              round="roundX2"
-              accentColor="#6374ae"
+          </CalendarProvider>
+          <CalendarProvider locale="fa" round="roundX4" accentColor="#6374ae">
+            <Calendar
+              value={new Date()}
+              onChange={(d) => console.log(d)}
+              weekends={[6]}
+              from="Mon Mar 06 2023 20:11:58 GMT+0330 (Iran Standard Time)"
+              to="Wed Mar 14 2023 21:41:23 GMT+0330 (Iran Standard Time)"
               range
             />
-          </div>
-          : null
-      }
+          </CalendarProvider>
+        </div>
+          )
+        : null}
+      {view === 'day'
+        ? (
+        <div className="libWrapper">
+          <DatePicker round="roundX4" />
+          <DatePicker round="roundX2" accentColor="#6374ae" range />
+        </div>
+          )
+        : null}
     </section>
   )
 }
