c9997e4b7d405ae1c03dab6391582558d0b0f362
rzkhosroshahi <rzkhkosroshahi@gmail.com>
2023-03-27 13:56:14 +0330

    chore: change the version

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { default as DatePicker } from './packages/DatePicker'
 export { default as CalendarProvider } from './packages/CalendarProvider'
 export { default as Calendar } from './packages/Calendar'
+export { default as TimePicker } from './packages/TimePicker'
